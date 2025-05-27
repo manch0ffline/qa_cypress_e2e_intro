29601ca3934cff2015ec5c49aba16e5912fcdf91
Roman Romanchuk <roma.roman4uk.48@gmail.com>
2025-05-27 21:04:09 +0200

    solution

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-    }
+    baseUrl: 'https://conduit.mate.academy/',
+    defaultCommandTimeout: 8000,
+    setupNodeEvents(on, config) {}
   }
 });
--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,74 @@
 /// <reference types="cypress" />
+import { faker } from '@faker-js/faker';
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
+  const randomDigits = Math.random().toString().slice(2, 6);
+  const username = faker.person.lastName().toLocaleLowerCase();
+  const email = faker.internet.email().toLocaleLowerCase();
+  const password = faker.internet.password() + randomDigits;
+  //   // register
+
+  //   cy.contains('a', 'Sign up').click();
+
+  //   cy.url().should('include', '/user/register');
+
+  //   cy.get('[placeholder="Username"]').should('be.visible').type(userName);
+  //   cy.get('[placeholder="Email"]').should('be.visible').type(email);
+  //   cy.get('[placeholder="Password"]').should('be.visible').type(password);
+
+  //   cy.get('button').contains('Sign up').click();
+
+  //   cy.contains('a', 'Setting').click();
+  //   cy.get('button').contains('Or click here to logout.').click();
+
+  //   // login
+
+  //   cy.contains('a', 'Sign in').click();
+
+  //   cy.url().should('include', '/user/login');
+
+  //   cy.get('[placeholder="Email"]').should('be.visible').type(email);
+  //   cy.get('[placeholder="Password"]').should('be.visible').type(password);
+
+  //   cy.get('button').contains('Sign in').click();
+  // });
+
+  // it('should provide the opportunity to register', () => {
+  //   cy.contains('a', 'Sign up').click();
+
+  //   cy.url().should('include', '/user/register');
+
+  //   cy.get('[placeholder="Username"]').should('be.visible').type(userName);
+  //   cy.get('[placeholder="Email"]').should('be.visible').type(email);
+  //   cy.get('[placeholder="Password"]').should('be.visible').type(password);
+
+  //   cy.get('button').contains('Sign up').click();
+
+  //   cy.contains('a', 'Setting').click();
+  //   cy.get('button').contains('Or click here to logout.').click();
+  // });
+
+  it('should provide an ability to log in', () => {
+    cy.request('POST', 'https://conduit.mate.academy/api/users', {
+      user: {
+        username,
+        email,
+        password
+      }
+    });
+
+    cy.contains('a', 'Sign in').click();
+
+    cy.url().should('include', '/user/login');
+
+    cy.get('[placeholder="Email"]').should('be.visible').type(email);
+    cy.get('[placeholder="Password"]').should('be.visible').type(password);
+
+    cy.get('button').contains('Sign in').click();
+    cy.contains('a', `${username}`).should('exist');
   });
 });
